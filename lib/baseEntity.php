0b486b933ce814647a6a9ba0f2008d8f71c11023
neilrichter <me@neilrichter.com>
2018-12-19 08:58:41 +0100

    ✨ Can now check if value exists + multiple where

--- lib/baseEntity.php
+++ lib/baseEntity.php
@@ -28,17 +28,22 @@ abstract class baseEntity {
         return $object;
     }
 
-    public function setConnexion($conn)
+    public function setConnexion($conn): void
     {
         $this->conn = $conn;
     }
 
-    public function getClassName()
+    public function getQueryBuilder(): \Doctrine\DBAL\Query\QueryBuilder
+    {
+        return $this->conn->createQueryBuilder();
+    }
+
+    public function getClassName(): String
     {
         return (new \ReflectionClass($this))->getShortName();
     }
 
-    public function arrayToObject(Array $data)
+    public function arrayToObject(Array $data): Array
     {
         for ($i = 0; $i < count($data); $i++) {
             $data[$i] = new $this->entityName($data[$i]);
@@ -46,7 +51,7 @@ abstract class baseEntity {
         return $data;
     }
 
-    public function getById(Int $id)
+    public function getById(Int $id): self
     {
         $qb = $this->conn->createQueryBuilder();
         $data = $qb
@@ -59,7 +64,7 @@ abstract class baseEntity {
         return new $this->entityName($data);
     }
 
-    public function getAll()
+    public function getAll(): Array
     {
         $qb = $this->conn->createQueryBuilder();
         $data = $qb
@@ -71,7 +76,7 @@ abstract class baseEntity {
         return $this->arrayToObject($data);
     }
 
-    public function getAllBy(String $property, String $order)
+    public function getAllBy(String $property, String $order): Array
     {
         $qb = $this->conn->createQueryBuilder();
         $data = $qb
@@ -84,16 +89,62 @@ abstract class baseEntity {
         return $this->arrayToObject($data);
     }
 
-    public function count() : Int
+    public function count(): Int
     {
         $qb = $this->conn->createQueryBuilder();
         $count = $qb
-            ->select('*')
+            ->select('id')
             ->from($this->getClassName())
             ->execute()
             ->rowCount();
         return $count;
-        
+    }
+
+    /**
+     * Checks if a value exists with the given data
+     * @param QueryBuilder|Array $mixed Querybuilder created by the user or an associative array colum => value
+     * @return bool
+     */
+    public function existsWith($mixed): Bool
+    {
+        $this->getQueryBuilder();
+        if (gettype($mixed) == 'array') {
+            $query = $this->ExistsWithArray($mixed);
+        } elseif ((new \ReflectionClass($mixed))->getShortName() == 'QueryBuilder') {
+            $query = $mixed;
+        } else {
+            throw new Exception('Invalid value type');
+        }
+        $exists = $query->execute()->fetch();
+        return !!$exists;
+    }
+
+    private function ExistsWithArray(Array $datas): \Doctrine\DBAL\Query\QueryBuilder
+    {
+        $qb = $this->conn->createQueryBuilder();
+        $qb
+            ->select('id')
+            ->from($this->getClassName());
+
+        foreach ($datas as $key => $value) {
+            $qb->andWhere("$key = :$key");
+            $qb->setParameter(":$key", $value);
+        }
+        return $qb;
+    }
+
+    public function selectAllWith($mixed): Array
+    {
+        $this->getQueryBuilder();
+        if (gettype($mixed) == 'array') {
+            $query = $this->ExistsWithArray($mixed);
+        } elseif ((new \ReflectionClass($mixed))->getShortName() == 'QueryBuilder') {
+            $query = $mixed;
+        } else {
+            throw new Exception('Invalid value type');
+        }
+        $rows = $query->execute()->fetch();
+        return $rows;
     }
 
     public function getAttributes()
